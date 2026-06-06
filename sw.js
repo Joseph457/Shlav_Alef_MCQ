8552b075d4fb159c3313171d577b9935ab5ba8f0
Yossi Levi <yossi.levi15@gmail.com>
2026-06-06 17:55:10 +0000

    App: archive questions, answered-today + 10-Q streak logic, Neligan in header, fuller offline precache (cache v26, v3.5)

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const C="shlav-mcq-v25";
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(["./","index.html","questions.json","manifest.webmanifest"])));});
+const C="shlav-mcq-v26";
+const PRE=["./","index.html","questions.json","manifest.webmanifest","icon-192.png","icon-512.png","images/N-I-8-13.jpg","images/N-I-8-30.jpg","images/N-I-8-7.jpg"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>Promise.all(PRE.map(u=>c.add(u).catch(()=>{})))));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp));return r;}).catch(()=>caches.match(e.request)));});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp));return r;}).catch(()=>caches.match(e.request).then(m=>m||caches.match("index.html"))));});
