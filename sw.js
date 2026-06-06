c19c3f6d47cd96b7ae82e44fce216ebdd9bbddb5
Yossi Levi <yossi.levi15@gmail.com>
2026-06-06 22:18:40 +0000

    Bologna Ch108: add 13 image-recognition MCQs (B108.61-73) with real figures; cache v32, ?b=32, v4.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C="shlav-mcq-v31";
-const PRE=["./","index.html","questions.json","manifest.webmanifest","icon-192.png","icon-512.png","images/N-I-8-13.jpg","images/N-I-8-30.jpg","images/N-I-8-7.jpg"];
+const C="shlav-mcq-v32";
+const PRE=["./","index.html","questions.json","manifest.webmanifest","icon-192.png","icon-512.png","images/N-I-8-13.jpg","images/N-I-8-30.jpg","images/N-I-8-7.jpg","images/B108-61.jpg","images/B108-62.jpg","images/B108-63.jpg","images/B108-64.jpg","images/B108-65.jpg","images/B108-66.jpg","images/B108-67.jpg","images/B108-68.jpg","images/B108-69.jpg","images/B108-70.jpg","images/B108-71.jpg","images/B108-72.jpg","images/B108-73.jpg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>Promise.all(PRE.map(u=>c.add(u).catch(()=>{})))));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp));return r;}).catch(()=>caches.match(e.request).then(m=>m||caches.match("index.html"))));});
